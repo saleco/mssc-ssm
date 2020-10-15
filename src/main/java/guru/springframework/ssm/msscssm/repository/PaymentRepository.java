c8db6f9f6320388bfb7417291814e1747bead974
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-10-15 11:30:52 +0100

    Creating payment model / repository

---
+++ src/main/java/guru/springframework/ssm/msscssm/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.ssm.msscssm.repository;
+
+import guru.springframework.ssm.msscssm.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
