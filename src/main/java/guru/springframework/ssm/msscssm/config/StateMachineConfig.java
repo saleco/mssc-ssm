cf05c77d571846bd0f02c035143425c9c42c7eb0
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-10-15 11:51:05 +0100

    Defininf states machine configuration with inital, end and states

---
+++ src/main/java/guru/springframework/ssm/msscssm/config/StateMachineConfig.java
@@ -0,0 +1,25 @@
+package guru.springframework.ssm.msscssm.config;
+
+import guru.springframework.ssm.msscssm.domain.PaymentEvent;
+import guru.springframework.ssm.msscssm.domain.PaymentState;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@EnableStateMachineFactory
+@Configuration
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.PRE_AUTH_ERROR)
+                .end(PaymentState.AUTH_ERROR);
+    }
+}
