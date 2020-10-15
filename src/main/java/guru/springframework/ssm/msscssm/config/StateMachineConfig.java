0504a8a26452185bd3b08c440201e33c11c3ce5d
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-10-15 18:10:07 +0100

    Including guard to check if the header payment_id is filled up

--- src/main/java/guru/springframework/ssm/msscssm/config/StateMachineConfig.java
+++ src/main/java/guru/springframework/ssm/msscssm/config/StateMachineConfig.java
@@ -12,6 +12,7 @@ import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.guard.Guard;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
@@ -38,7 +39,7 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
 
         //describing event transtions
         transitions
-                .withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE).action(preAuthAction())
+                .withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE).action(preAuthAction()).guard(paymentIdGuard())
                 .and()
                 .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
                 .and()
@@ -62,6 +63,10 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
             }
         };
         config.withConfiguration().listener(adapter);
+    }
+
+    public Guard<PaymentState, PaymentEvent> paymentIdGuard() {
+        return context -> context.getMessageHeader(PaymentServiceImpl.PAYMENT_ID_HEADER) != null;
     }
 
     public Action<PaymentState, PaymentEvent> preAuthAction() {
