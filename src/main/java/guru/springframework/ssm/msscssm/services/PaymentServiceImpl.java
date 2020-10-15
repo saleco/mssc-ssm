720b6388e439ec79912524caff7dccbc66b2d1e7
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-10-15 13:10:13 +0100

    Restarting state machine with the payment status

--- src/main/java/guru/springframework/ssm/msscssm/services/PaymentServiceImpl.java
+++ src/main/java/guru/springframework/ssm/msscssm/services/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import guru.springframework.ssm.msscssm.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -24,16 +25,34 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
         return null;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+        Payment payment = paymentRepository.getOne(paymentId);
+        StateMachine<PaymentState, PaymentEvent> stateMachine = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+        stateMachine.stop();
+
+        stateMachine.getStateMachineAccessor()
+            .doWithAllRegions(sma -> {
+                sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+            });
+
+        stateMachine.start();
+        return stateMachine;
+
     }
 }
