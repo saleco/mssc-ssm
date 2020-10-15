e63491417f758ca769a63ee57cc33a62bf11d9cb
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-10-15 14:04:35 +0100

    Creating pre authrize action when transitioning to pre authorize event

--- src/test/java/guru/springframework/ssm/msscssm/services/PaymentServiceImplTest.java
+++ src/test/java/guru/springframework/ssm/msscssm/services/PaymentServiceImplTest.java
@@ -42,7 +42,7 @@ class PaymentServiceImplTest {
 
         Payment preAuthedPayment = paymentRepository.getOne(savedPayment.getId());
 
-        System.out.println("Should be PRE_AUTH");
+        System.out.println("Should be PRE_AUTH or PRE_AUTH_ERROR");
         System.out.println(stateMachine.getState().getId());
 
         System.out.println(preAuthedPayment);
